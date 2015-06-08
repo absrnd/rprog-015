d81afc0347afd6938d88de56d0a42f8af90aeadd
Stuart Malcolm <stuart@ifoundry.co.uk>
2015-06-08 17:27:51 +0100

    template code and data for part 1 of assignment 1

---
+++ assignment-01/pollutantmean.R
@@ -0,0 +1,43 @@
+# pollutantmean.R - Part 1 of Assignment 1 Coursera R Programming
+#
+# A function that calculates the mena of a pollutant (sulfate or nitrate)
+# across a specified list of monitors.
+#
+# Given a vector of monitor ID numbers, return the mean of the pollutant
+# across all of the monitors, ignoring missing values coded as NA.
+#
+# The specdata directory contains 322 CSV files containg pollutant data:
+#
+# |------------+---------+---------+----|
+# | Date       | sulfate | nitrate | ID |
+# |------------+---------+---------+----|
+# | 01/01/2004 | NA      | NA      | 12 |
+# | 02/01/2004 | NA      | NA      | 12 |
+# | 28/03/2004 | 0.0353  | 0.0598  | 12 |
+# | 08/04/2004 | NA      | NA      | 12 |
+# | 09/04/2004 | 1.33    | 0.95    | 12 |
+# | ...        |         |         |    |
+# |------------+---------+---------+----|
+#
+#
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+
+        ## 'pollutant' is a character vector of length 1 indicating
+        ## the name of the pollutant for which we will calculate the
+        ## mean; either "sulfate" or "nitrate".
+
+        ## 'id' is an integer vector indicating the monitor ID numbers
+	## to be used
+
+	## Return the mean of the pollutant across all monitors list
+	## in the 'id' vector (ignoring NA values)
+	## NOTE: Do not round the result!
+
+
+	# 1. Open the input file
+	# 2. remove all non-missing rows
+	#
+
+}
