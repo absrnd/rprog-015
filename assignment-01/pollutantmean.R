e412b10ee1e96dd2055c03ab67fede4016bab53f
Stuart Malcolm <stuart@ifoundry.co.uk>
2015-06-09 15:27:10 +0100

    updated after part 1 submitted

--- assignment-01/pollutantmean.R
+++ assignment-01/pollutantmean.R
@@ -21,23 +21,36 @@
 #
 #
 pollutantmean <- function(directory, pollutant, id = 1:332) {
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
 
-        ## 'pollutant' is a character vector of length 1 indicating
-        ## the name of the pollutant for which we will calculate the
-        ## mean; either "sulfate" or "nitrate".
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
 
-        ## 'id' is an integer vector indicating the monitor ID numbers
-	## to be used
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
 
-	## Return the mean of the pollutant across all monitors list
-	## in the 'id' vector (ignoring NA values)
-	## NOTE: Do not round the result!
-
-
-	# 1. Open the input file
-	# 2. remove all non-missing rows
-	#
-
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    ## NOTE: Do not round the result!
+    values <- c()                                           # empty vector to accumulate result
+    for (i in 1:length(id)) {                               # for each input ID..
+        filename <- file.path(directory,paste0(sprintf("%03d",id[i]),".csv"))
+        all_data <- read.csv(file=filename,colClasses=c("character","numeric","numeric","numeric"))
+        #print(paste(filename,"has",nrow(all_data),"total rows"))
+        id_data <- all_data #[which(all_data$ID==i),]         # ..select ID that match
+        #print(paste(filename,"has",nrow(id_data),"ID rows"))
+        all_pollutant <- id_data[[pollutant]]               # ..and only pollutant values
+        #print(paste(filename,"has",nrow(all_pollutant),"pollutant rows"))
+        nonmiss <- all_pollutant[!is.na(all_pollutant)]     # ..which are not missing
+        #print(paste(filename,"has",length(nonmiss),"non missing values"))
+        ## accumulate this ID results with all other results
+        values <- c(nonmiss,values)
+        
+    }
+    ## return the mean of the accumulated values
+    mean(values,na.rm=TRUE)
+    
 }
+
